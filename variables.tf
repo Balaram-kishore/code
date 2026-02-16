fe1c24cdc4d8f737ada514684073e6785f8a86d7
Balaramkishore <gbkishore@gmail.com>
2026-02-15 23:04:39 -0500

    new change

--- variables.tf
+++ variables.tf
@@ -2,9 +2,5 @@
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
-<<<<<<< HEAD
   default     = "10.0.0.0/16"
 }
-=======
-  default     = "10.0.0.0/16"
->>>>>>> f3691dac0ab1c474897fba7cf472db3e58d259a2
